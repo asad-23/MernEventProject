47557b45e0b354e4373cb4fdb13aafc354c58395
asad-23 <140097856+asad-23@users.noreply.github.com>
2024-12-05 23:55:12 +0600

    Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='bg-[#3A1078] text-white pb-8'>
       <div className='max-w-[1500px] mx-auto'>
-        <h1 className='text-3xl font-bold text-center p-4'>Vadaimma</h1>
+        <h1 className='text-3xl font-bold text-center p-4'>WELCOME</h1>
         <p className='text-2xl text-center'>Your Personal Dream Maker</p>
         <p className='text-2xl text-center'>Powered and Sponsered by Me</p>
       </div>
